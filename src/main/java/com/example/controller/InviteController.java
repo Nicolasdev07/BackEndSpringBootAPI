961d969c871efe0f0b0ca0f519363159261862b9
Nicolas ARNAUDON <nicolas.arnaudon@hotmail.fr>
2022-12-07 21:39:43 +0100

    V1

--- src/main/java/com/example/controller/InviteController.java
+++ src/main/java/com/example/controller/InviteController.java
@@ -18,8 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@CrossOrigin(origins = "https://mariage-clemence-nicolas.netlify.app/")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class InviteController {
